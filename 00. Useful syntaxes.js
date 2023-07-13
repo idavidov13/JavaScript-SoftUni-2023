63ae51beb27de3c8900116456daca4f275d3568f
idavidov13 <118520300+idavidov13@users.noreply.github.com>
2023-07-13 17:03:10 +0300

    Create 00. Useful syntaxes.js

---
+++ 00. Useful syntaxes.js
@@ -0,0 +1,38 @@
+// For loop
+for (initialization; condition; afterthough) {
+  statement
+};
+
+// Example
+for(let i=1; i<10; i++) {
+  console.log(i);
+};
+
+// Iterating over arrays
+let number = [2, 3, 6];
+for (let i=0; i < numbers.length, i++) {
+  console.log(number[i]);
+};
+
+// If statements
+if (true) {
+....
+} else {
+....
+};
+
+// Example
+let age = 16;
+if (age >= 18) {
+  console.log("You are an adult");
+} else {
+  console.log("You are too young");
+};
+
+// Searching for data in an array with objects
+let isFound = false;
+for (let i=0; i < array.length; i++) {
+    if (array[i].attribute === searchedElement){
+       isFound = true;;
+    }
+}
