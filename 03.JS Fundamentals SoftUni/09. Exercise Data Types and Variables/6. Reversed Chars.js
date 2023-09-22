b639f3ba5ce354fd9901a74fbd73e577d64447c8
Ivan Davidov <ivandavidov13@yahoo.com>
2023-09-22 08:17:59 +0300

    task 6

---
+++ 03.JS Fundamentals SoftUni/09. Exercise Data Types and Variables/6. Reversed Chars.js
@@ -0,0 +1,7 @@
+// 6. Reversed Chars
+
+// Write a program that takes 3 parameters (characters) and prints them in reversed order with a space between them.
+function reverse(one, two, three) {
+  console.log(`${three} ${two} ${one}`);
+}
+reverse("A", "B", "C");
