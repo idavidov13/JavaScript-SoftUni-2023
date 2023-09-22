d7850ac4d7afe162d6f84e09cc35c8ea6eab8f07
Ivan Davidov <ivandavidov13@yahoo.com>
2023-09-22 16:15:41 +0300

    solve

---
+++ 03.JS Fundamentals SoftUni/00. More Excercises/01. Digits with Words.js
@@ -0,0 +1,36 @@
+// 1. Digits with Words
+// Write a function that receives a digit in the form of a word (as a string) and prints the digit (as a number)
+function rework(digit) {
+  switch (digit) {
+    case "one":
+      console.log(1);
+      break;
+    case "two":
+      console.log(2);
+      break;
+    case "three":
+      console.log(3);
+      break;
+    case "four":
+      console.log(4);
+      break;
+    case "five":
+      console.log(5);
+      break;
+    case "six":
+      console.log(6);
+      break;
+    case "seven":
+      console.log(7);
+      break;
+    case "eight":
+      console.log(8);
+      break;
+    case "nine":
+      console.log(9);
+      break;
+    case "zero":
+      console.log(0);
+      break;
+  }
+}
