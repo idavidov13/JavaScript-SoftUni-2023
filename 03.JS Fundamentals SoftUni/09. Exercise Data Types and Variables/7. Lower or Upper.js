e0f8e67a95773a17a0ebaff195a04eebe7b62f3b
Ivan Davidov <ivandavidov13@yahoo.com>
2023-09-22 08:21:28 +0300

    task 7

---
+++ 03.JS Fundamentals SoftUni/09. Exercise Data Types and Variables/7. Lower or Upper.js
@@ -0,0 +1,11 @@
+// 7. Lower or Upper
+
+// Write a function that prints whether a given character is upper-case or lower-case.
+function lowerOrUpper(char) {
+  if (char === char.toUpperCase()) {
+    console.log(`upper-case`);
+  } else {
+    console.log(`lower-case`);
+  }
+}
+lowerOrUpper("L");
