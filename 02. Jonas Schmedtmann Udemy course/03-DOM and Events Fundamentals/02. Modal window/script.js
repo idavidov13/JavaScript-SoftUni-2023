0e21dd69415081dd25c0ec95c48532c6895af01f
Ivan Davidov <ivandavidov13@yahoo.com>
2023-09-26 20:07:37 +0300

    completed modal project

---
+++ 02. Jonas Schmedtmann Udemy course/03-DOM and Events Fundamentals/02. Modal window/script.js
@@ -0,0 +1,27 @@
+"use strict";
+let modal = document.querySelector(".modal");
+let overlay = document.querySelector(".overlay");
+let btnCloseModal = document.querySelector(".close-modal");
+let btnShowModal = document.querySelectorAll(".show-modal");
+let openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+let closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnShowModal.length; i++) {
+  btnShowModal[i].addEventListener("click", openModal);
+}
+
+btnCloseModal.addEventListener("click", closeModal);
+
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
