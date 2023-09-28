df8d1848a6e20d9706a368fe02508705950693ca
Ivan Davidov <ivandavidov13@yahoo.com>
2023-09-28 17:41:12 +0300

    solved

---
+++ 03.JS Fundamentals SoftUni/11. Arrays/01. Sum First and Last Array Elements.js
@@ -0,0 +1,8 @@
+// 1. Sum First and Last Array Elements
+
+// Write a function that receives an array of numbers and prints the sum of the first and last element in that array.
+
+function solve(arr) {
+  console.log(arr[0] + arr[arr.length - 1]);
+}
+solve([20, 30, 40]);
