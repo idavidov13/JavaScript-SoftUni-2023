75c0c8dbe8264ead1c76652cdd2a27d1f663432b
Ivan Davidov <ivandavidov13@yahoo.com>
2023-10-07 11:51:14 +0300

    solved

---
+++ 03.JS Fundamentals SoftUni/15. Exercise Functions/03. Characters in Range.js
@@ -0,0 +1,19 @@
+// 3. Characters in Range
+
+// Write a function that receives two characters and prints on a single line all the characters in between them according to the ASCII code. Keep in mind that the second character code might be before the first one inside the ASCII table.
+function charInRange(char1, char2) {
+  let code1 = char1.charCodeAt(0);
+  let code2 = char2.charCodeAt(0);
+  let min = code1;
+  let max = code2;
+  let array = [];
+  if (min > max) {
+    min = code2;
+    max = code1;
+  }
+  for (let i = min + 1; i < max; i++) {
+    array.push(String.fromCharCode(i));
+  }
+  console.log(array.join(" "));
+}
+charInRange("a", "d");
