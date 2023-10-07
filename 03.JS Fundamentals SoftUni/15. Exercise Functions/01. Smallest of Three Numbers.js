efe63c0e5d178db64de633f1caaf7537dbed25a7
Ivan Davidov <ivandavidov13@yahoo.com>
2023-10-07 09:47:23 +0300

    solved

---
+++ 03.JS Fundamentals SoftUni/15. Exercise Functions/01. Smallest of Three Numbers.js
@@ -0,0 +1,14 @@
+// 1. Smallest of Three Numbers
+
+// Write a function that receives three integers and prints the smallest number. Use an appropriate name for the function.
+function smallestNumber(num1, num2, num3) {
+  let smallestNumber = num1;
+  if (num2 < smallestNumber) {
+    smallestNumber = num2;
+  }
+  if (num3 < smallestNumber) {
+    smallestNumber = num3;
+  }
+  console.log(smallestNumber);
+}
+smallestNumber(2, 5, 3);
