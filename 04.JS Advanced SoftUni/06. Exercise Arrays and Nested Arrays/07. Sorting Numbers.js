66cefd4f682e15a8bb2240e9b338cdcf468fe7b4
Ivan Davidov <ivandavidov13@yahoo.com>
2024-01-16 09:25:15 +0200

    solved

---
+++ 04.JS Advanced SoftUni/06. Exercise Arrays and Nested Arrays/07. Sorting Numbers.js
@@ -0,0 +1,19 @@
+// 7. Sorting Numbers
+
+// Write a function that sorts an array of numbers so that the first element is the smallest one, the second is the biggest one, the third is the second smallest one, the fourth is the second biggest one, and so on.
+
+// Return the resulting array.
+function sort(arr) {
+  let arrForPrint = [];
+  arr.sort((a, b) => a - b);
+  while (arr.length > 0) {
+    let smallest = arr.shift();
+    arrForPrint.push(smallest);
+    if (arr.length > 0) {
+      let biggest = arr.pop();
+      arrForPrint.push(biggest);
+    }
+  }
+  console.log(arrForPrint);
+}
+sort([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]);
