5709dac2ac8e4670b0a08304bbfb55d676e55b79
Ivan Davidov <ivandavidov13@yahoo.com>
2024-01-31 10:38:34 +0200

    refactored

--- 04.JS Advanced SoftUni/12. Exercise DOM Manipulations and Events/01. Sections/script.js
+++ 04.JS Advanced SoftUni/12. Exercise DOM Manipulations and Events/01. Sections/script.js
@@ -2,8 +2,10 @@
 
 // You will receive an array of strings. For each string, create a div with a paragraph with the string in it. Each paragraph is initially hidden (display:none). Add a click event listener to each div that displays the hidden paragraph. Finally, you should append all divs to the element with an id "content".
 function create(words) {
+  //Take the parent element to which we will append childs
   let appendHere = document.getElementById("content");
 
+  // Loop through the input to create div and p and set their properties. Add an eventListener that will change the display style
   for (let each of words) {
     let newDiv = document.createElement("div");
     let newP = document.createElement("p");
@@ -18,4 +20,31 @@ function create(words) {
       newP.style.display = "block";
     });
   }
+}
+
+//Refactored with the creation of new function
+function create(words) {
+  //Take the parent element to which we will append childs
+  let appendHere = document.getElementById("content");
+
+  // Loop through the input to create div and p and set their properties. Add an eventListener that will change the display style
+  for (let each of words) {
+    let newDiv = document.createElement("div");
+    let newP = document.createElement("p");
+
+    newP.style.display = "none";
+    newP.textContent = each;
+
+    newDiv.appendChild(newP);
+    appendHere.appendChild(newDiv);
+
+    newDiv.addEventListener("click", clickHandler);
+  }
+}
+
+function clickHandler(event) {
+  let target = event.currentTarget;
+  let children = target.children;
+  let p = children[0];
+  p.style.display = "block";
 }
