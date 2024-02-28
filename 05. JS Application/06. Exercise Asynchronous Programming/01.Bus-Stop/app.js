0c83c8c86b30c1fd5bf2012551083e206a10e99c
Ivan Davidov <ivandavidov13@yahoo.com>
2024-02-28 07:36:36 +0200

    refactored

--- 05. JS Application/06. Exercise Asynchronous Programming/01.Bus-Stop/app.js
+++ 05. JS Application/06. Exercise Asynchronous Programming/01.Bus-Stop/app.js
@@ -57,3 +57,33 @@ function getInfo() {
     }
   }
 }
+
+//Refactored with Async / Await
+async function getInfo() {
+  const stopIdRef = document.getElementById("stopId");
+  const stopId = stopIdRef.value;
+  const stopNameRef = document.getElementById("stopName");
+  const busesRef = document.getElementById("buses");
+
+  stopNameRef.textContent = "";
+  busesRef.innerHTML = "";
+
+  const url = `http://localhost:3030/jsonstore/bus/businfo/${stopId}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    stopNameRef.textContent = data.name;
+    appendChild(Object.entries(data.buses));
+  } catch (err) {
+    stopNameRef.textContent = "Error";
+  }
+
+  function appendChild(data) {
+    for ([bus, time] of data) {
+      let element = document.createElement("li");
+      element.textContent = `Bus ${bus} arrives in ${time} minutes`;
+      busesRef.appendChild(element);
+    }
+  }
+}
