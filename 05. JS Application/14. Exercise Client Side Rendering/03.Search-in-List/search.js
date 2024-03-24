f8f2ed77996bb79a881d8c49967354e3c0f6ccdc
Ivan Davidov <ivandavidov13@yahoo.com>
2024-03-24 08:38:53 +0200

    solved

---
+++ 05. JS Application/14. Exercise Client Side Rendering/03.Search-in-List/search.js
@@ -0,0 +1,35 @@
+import { html, render } from "./node_modules/lit-html/lit-html.js";
+import { towns } from "./towns.js";
+
+document.querySelector("button").addEventListener("click", search);
+
+const townRoot = document.getElementById("towns");
+const root = document.getElementById("result");
+const inputRef = document.getElementById("searchText");
+
+update();
+
+function update(match) {
+  render(ulTemplate(towns, match), townRoot);
+}
+
+function ulTemplate(towns, match) {
+  return html`<ul>
+    ${towns.map((town) => createLiTemplate(town, match?.includes(town)))}
+  </ul>`;
+}
+
+function createLiTemplate(town, isActive) {
+  return html`<li class=${isActive ? "active" : ""}>${town}</li>`;
+}
+
+function search() {
+  const searchText = inputRef.value;
+  const match = towns.filter((town) => town.includes(searchText));
+  update(match);
+  renderMatchCount(match.length);
+}
+
+function renderMatchCount(count) {
+  render(html`${count} matches found`, root);
+}
