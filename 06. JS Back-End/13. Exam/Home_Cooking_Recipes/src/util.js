3c2f0c56adcc1f199407fa24896313d171134e4e
Ivan Davidov <ivandavidov13@yahoo.com>
2024-06-23 18:20:58 +0300

    final version

--- 06. JS Back-End/13. Exam/Home_Cooking_Recipes/src/util.js
+++ 06. JS Back-End/13. Exam/Home_Cooking_Recipes/src/util.js
@@ -1,10 +1,8 @@
 function parseError(err) {
   if (err instanceof Error) {
     if (!err.errors) {
-      //Generic error
       err.errors = [err.message];
     } else {
-      //Mongoose validation error
       const error = new Error("Input validation error");
       error.errors = Object.fromEntries(
         Object.values(err.errors).map((e) => [e.path, e.message])
@@ -13,7 +11,6 @@ function parseError(err) {
       return error;
     }
   } else if (Array.isArray(err)) {
-    // Express-validator error array
     const error = new Error("Input validation error");
     error.errors = Object.fromEntries(err.map((e) => [e.path, e.msg]));
 
